ea825219014d246e965eab1a3b8707a041a48a0e
t0yman <195047463+t0yman@users.noreply.github.com>
2025-10-23 14:19:19 -0700

    fixed incorrect offset parameter passed to enable color attribute

--- source/main.cpp
+++ source/main.cpp
@@ -68,7 +68,7 @@ int main()
     glEnableVertexAttribArray(0);
 
     // enable color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
