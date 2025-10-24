3523f9a470aaa57ca1e55ed71ca4389f5f06509d
t0yman <195047463+t0yman@users.noreply.github.com>
2025-10-24 16:26:54 -0700

    refactored to implement 3D transforms

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,13 @@
+#include <iostream>
 #include <stdexcept>
 
 #include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void FramebufferSizeCallback(GLFWwindow* windowHandle, int width, int height);
 void ProcessInput(GLFWwindow* windowHandle);
@@ -49,9 +55,9 @@ int main()
 
     // layout: position.x, position.y, position.z, color.r, color.g, color.b 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+        -0.5f, -0.5f, 0.7f, 1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.3f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f
     };
 
     unsigned int vao;
@@ -81,9 +87,14 @@ int main()
 
         out vec3 vertexColor;
 
+        uniform mat4 modelMatrix;
+        uniform mat4 viewMatrix;
+        uniform mat4 projectionMatrix;
+
         void main()
         {
-            gl_Position = vec4(aPos, 1.0);
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(aPos, 1.0);
+
             vertexColor = aColor;
         }
     )";
@@ -111,7 +122,7 @@ int main()
     if (success != true)
     {
         glGetShaderInfoLog(vertexShader, 512, nullptr, log);
-
+        std::cerr << log << std::endl;
         throw std::runtime_error{"vertex shader compilation failed"};
     }
 
@@ -122,7 +133,7 @@ int main()
     if (success != true)
     {
         glGetShaderInfoLog(fragmentShader, 512, nullptr, log);
-
+        std::cerr << log << std::endl;
         throw std::runtime_error{"fragment shader compilation failed"};
     }
 
@@ -134,21 +145,45 @@ int main()
     if (success != true)
     {
         glGetProgramInfoLog(shaderProgram, 512, nullptr, log);
-
+        std::cerr << log << std::endl;
         throw std::runtime_error{"shader program linking failed"};
     }
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    const glm::vec3 cameraPos = glm::vec3{0.0f, 0.0f, 3.0f};
+    const glm::vec3 cameraTarget = glm::vec3{0.0f, 0.0f, 0.0f};
+    const glm::vec3 cameraUp = glm::vec3{0.0f, 1.0f, 0.0f};
+
+    float aspectRatio = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
+    const float fov = glm::radians(45.0f);
+    const float distanceToNearPlane = 0.1f;
+    const float distanceToFarPlane = 100.0f;
+
+    const int modelMatrixLocation = glGetUniformLocation(shaderProgram, "modelMatrix");
+    const int viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
+    const int projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projectionMatrix");
+
+    glEnable(GL_DEPTH_TEST);
+
     while (glfwWindowShouldClose(windowHandle) == false)
     {
         ProcessInput(windowHandle);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+
+        glm::mat4 modelMatrix = glm::mat4{1.0f};
+        glm::mat4 viewMatrix = glm::lookAt(cameraPos, cameraTarget, cameraUp);
+        glm::mat4 projectionMatrix = glm::perspective(fov, aspectRatio, distanceToNearPlane, distanceToFarPlane);
+
+        glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+        glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+        glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
